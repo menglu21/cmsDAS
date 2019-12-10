88d7cbdeaa88d3fe1018031618ebe5625842caa9
Meng_Lu <1501110102@pku.edu.cn>
2019-12-10 11:34:00 +0800

    Update run_cfg.py

--- Exercise1/run_cfg.py
+++ Exercise1/run_cfg.py
@@ -9,8 +9,8 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(40) )
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        #'file:/home/cmsdas/lum/cmsDAS/CMSSW_10_2_18/src/photonAna/photonAnalyzer/ZG1.root'
-        'file:/home/cmsdas/lum/cmsDAS/CMSSW_10_2_18/src/photonAna/photonAnalyzer/DY1.root'
+        'file:/home/cmsdas/lum/cmsDAS/CMSSW_10_2_18/src/photonAna/photonAnalyzer/ZG1.root'
+        #'file:/home/cmsdas/lum/cmsDAS/CMSSW_10_2_18/src/photonAna/photonAnalyzer/DY1.root'
     )
 )
 
